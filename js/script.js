b3d1fff0d50e9e79e025ee59819881886f90379e
sitoraymarova@gmail.com <sitoraymarova@gmail.com>
2023-06-01 17:59:47 +0500

    new file

---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+    //  FLOOR
+function floor(number) {
+    let total = Math.floor(number);
+    return total;
+}
+
+let total = floor(-5,3);
+console.log(total);
+
+
+    //   CEIL
+function ceil(num) {
+    let result = Math.ceil(num);
+    return result;
+}
+        
+let result = ceil(5,3);
+console.log(result);
+
+
+        // ROUND
+function round(num_2) {
+    // let result_2 = Math.round(num_2);
+    return Math.round(num_2);
+}
+let result_2 = round(5.34598566);
+console.log(result_2);
+
+
+    //   TRUNC
+function trunc(num_3) {
+    return Math.trunc(num_3);
+}
+let result_3 = trunc(8.7686565);
+console.log(result_3);
+
+
+
+        //    POW
+function pow(a, b) {
+    return Math.pow(a, b);
+}
+let result_4 = pow(8, 4);
+console.log(result_4);
+
+
+        // ABC
+function Abs(num_4) {
+    return Math.abs(num_4);
+}
+          
+let result_5 = Abs(-8);
+console.log(result_5);
+
+
+        // SQRT
+function sqrt(num_5) {
+    return Math.sqrt(num_5);
+}
+  
+let result_6 = sqrt(25);
+console.log(result_6);
+  
+          
